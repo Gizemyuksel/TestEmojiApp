8b6b52abf8804d9ad53ad8643faba9900ad20abe
gizemyuksel <gizem.682@windowslive.com>
2024-10-23 01:17:03 +0300

    test1

---
+++ TestEmojiApp/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  TestEmojiApp
+//
+//  Created by Gizem Yüksel on 23.10.2024.
+//
+
+import SwiftUI
+
+enum Emoji: String, CaseIterable {
+    case 👩‍💻, 🚀, 🦄, 🥂
+}
+
+struct ContentView: View {
+    @State var selection: Emoji = .👩‍💻
+    
+    var body: some View {
+        NavigationView {
+            VStack{
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases, id: \.self) { emoji in Text(emoji.rawValue)
+                    }
+                    
+                }
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle("Pick Your Emoji!")
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestEmojiApp/TestEmojiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestEmojiAppApp.swift
+//  TestEmojiApp
+//
+//  Created by Gizem Yüksel on 23.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestEmojiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
